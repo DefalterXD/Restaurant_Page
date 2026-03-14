b4ee1f499f8301c8fb0c19f0025d565f692bb751
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-03-14 20:36:45 +0500

    feat: render the pages based on clicked button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./reset.css";
 import "./style.css";
+import { mainNodes } from "./pages/domCreate.js";
+import { createHomePage } from "./pages/home.js";
+import { createMenuPage } from "./pages/menu.js";
+import { createAboutPage } from "./pages/about.js";
+
+mainNodes.nav.addEventListener('click', (e) => {
+    const headerLink = e.target;
+
+    if (headerLink.tagName !== 'BUTTON') return;
+
+    if (headerLink.className === 'home') {
+        mainNodes.content.clear();
+        createHomePage();
+    } else if (headerLink.className === 'menu') {
+        mainNodes.content.clear();
+        createMenuPage();
+    } else {
+        mainNodes.content.clear();
+        createAboutPage();
+    }
+});
+
+
+createHomePage();
