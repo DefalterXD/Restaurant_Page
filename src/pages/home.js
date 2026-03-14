f7af258488a0b745d80615fe67cd6aaeed522d3e
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-03-14 20:32:19 +0500

    feat: create DOM elements and add them into array

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,11 @@
 import { mainNodes, ElementObj } from "./domCreate.js";
 import background from "../img/background.jpg";
 
+const homeElementsData = [];
+homeElementsData.push(ElementObj.createDOM(new ElementObj("section", "title__container")));
+homeElementsData.push(ElementObj.createDOM(new ElementObj("h2", "greeting", 'Welcome to')));
+homeElementsData.push(ElementObj.createDOM(new ElementObj("h1", "restaurant__title", 'Restaurant')));
+homeElementsData.push(ElementObj.createDOM(new ElementObj("button", "menu", 'See menu')));
 
 
 const createHomePage = function createHomePageWithinTheContent() {
