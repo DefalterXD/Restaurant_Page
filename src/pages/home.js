c8e1e1cd88f42f4a05d4f7dc0bdfd57e2d8ebe5b
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-03-14 20:33:15 +0500

    feat: append all the elements into DOM

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,11 +7,18 @@ homeElementsData.push(ElementObj.createDOM(new ElementObj("h2", "greeting", 'Wel
 homeElementsData.push(ElementObj.createDOM(new ElementObj("h1", "restaurant__title", 'Restaurant')));
 homeElementsData.push(ElementObj.createDOM(new ElementObj("button", "menu", 'See menu')));
 
+const section = homeElementsData[0];
 
 const createHomePage = function createHomePageWithinTheContent() {
     mainNodes.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${background})`;
 
+    homeElementsData.forEach((element) => {
+        if (element !== section) {
+            section.appendChild(element);
+        }
+    });
         
+    mainNodes.content.divContent.appendChild(section);
     
 };
 
