cb7b99a4ad405978a000157bf76f42022102bf60
Defalter <103436489+DefalterXD@users.noreply.github.com>
2026-03-14 20:31:16 +0500

    feat: apply background for 'home' page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,13 @@
 import { mainNodes, ElementObj } from "./domCreate.js";
+import background from "../img/background.jpg";
+
+
 
 const createHomePage = function createHomePageWithinTheContent() {
+    mainNodes.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${background})`;
+
+        
+    
 };
 
 export { createHomePage };
